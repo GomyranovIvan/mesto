63ef3a51a8fb101b07c602d742d38b950745fbba
Gomyranov Ivan <ffreedom37@gmail.com>
2023-03-06 23:50:57 +0700

    go2

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,29 @@
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".profile__button-edit");
-const formElement = document.querySelector(".popup__content");
-const nameInput = formElement.querySelector(".popup__name");
-const jobInput = formElement.querySelector(".popup__description");
-const profileName = document.querySelector(".profile__name");
-const profileDescription = document.querySelector(".profile__description");
-const popupSaveButtonElement = popupElement.querySelector(".popup__save")
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+const formElement = document.querySelector('.popup__content');
+const nameInput = formElement.querySelector('.popup__input_name');
+const jobInput = formElement.querySelector('.popup__input_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
 const openPopup = function () {
-    popupElement.classList.add("popup_is-opened");
-    const AddName = profileName.textContent;
-    const AddDescription = profileDescription.textContent;
-    nameInput.value = AddName;
-    jobInput.value = AddDescription;
+    popupElement.classList.add('popup_opened');
+    const AddNamefromProfil = profileName.textContent;
+    const AddDescriptionfromProfil = profileDescription.textContent;
+    nameInput.value = AddNamefromProfil;
+    jobInput.value = AddDescriptionfromProfil;
 
 };
 
 const closePopup = function () {
-    popupElement.classList.remove("popup_is-opened");
-};
-
-const closePopupByClickOnOverlay = function (event) {
-    if (event.currentTarget !== event.target) {
-        return;
-    }
-    closePopup()
+    popupElement.classList.remove('popup_opened');
 };
 
 const saveValuesToProfile = function() {
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
 };
-
-popupOpenButtonElement.addEventListener('click', openPopup,);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
-popupSaveButtonElement.addEventListener('click', saveValuesToProfile);
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function handleFormSubmit (evt) {
@@ -46,17 +33,14 @@ function handleFormSubmit (evt) {
 
     // Получите значение полей jobInput и nameInput из свойства value
     // Выберите элементы, куда должны быть вставлены значения полей
-let profileName = document.querySelector(".profile__name");
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
-    
-
-let profileDescription = document.querySelector(".profile__description");
-
     profileDescription.textContent = jobInput.value;
-
+    closePopup()
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
+popupOpenButtonElement.addEventListener('click', openPopup,);
+popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
