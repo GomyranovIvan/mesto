e2a253c570ab07d9fcbad58d3895367dfcc8b6a9
Gomyranov Ivan <ffreedom37@gmail.com>
2023-06-24 01:13:13 +0300

    GH

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,66 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  // Точка входа
+  entry: { main: './src/scripts/index.js' },
+
+  // Точка выхода
   output: {
+    path: path.resolve(__dirname, 'dist'), // вызов path для получения абсолютного пути
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
   },
+
+  // Добавили режим разработчика
   mode: 'development',
+
+  // Настройки локального сервера
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 8081,
-    compress: true,
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // ускорение загрузки в режиме разработки
+    port: 8080, // порт вебсервера
+    open: true // сайт будет открываться сам при запуске npm run dev
   },
+
+  // Лоадеры (модули)
   module: {
-    rules: [
+    rules: [ 
+      // Babel
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/',
+        test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+        use: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
+        exclude: '/node_modules/' // исключает папку node_modules, файлы в ней обрабатывать не нужно
       },
+
+      // Изображения и шрифты
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource' // переносить исходные файлы в конечную сборку в том же формате
+      },
+
+      // MiniCssExtractPlugin.loader и css-loader
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
           loader: 'css-loader',
-          options: { importLoaders: 1 }
-          },
-    // Добавьте postcss-loader
-        'postcss-loader']
-       },
-       {
-        test:/\.(svg|jpg)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name].[hash][ext]',
-        }
-       },
-       {
-        test:/\.(woff|woff2)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name].[hash][ext]',
-        }
-       }
+          options: { importLoaders: 1 } // изменение порядка запуска, чтобы правильно работали импорты в css (сначала обработка в PostCSS)
+        },
+        'postcss-loader'] 
+      },
     ]
   },
+
+  devtool: 'inline-source-map',
+
+  // Плагины
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        template: './src/index.html'
+      template: './index.html' 
     }),
-    new MiniCssExtractPlugin({
-        filename: 'index.css'
-        }
-    ),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
   ]
-};
+}
